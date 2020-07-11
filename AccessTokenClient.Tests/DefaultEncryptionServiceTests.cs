1847a66efd65a517749ecf7c5bd5750180ec5ddc
Bill Applegate <bill.applegate@gmail.com>
2020-07-11 10:37:18 -0400

    Added additional unit tests.

---
+++ AccessTokenClient.Tests/DefaultEncryptionServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using AccessTokenClient.Encryption;
+using AccessTokenClient.Tests.Helpers;
+using FluentAssertions;
+using Xunit;
+
+namespace AccessTokenClient.Tests
+{
+    public class DefaultEncryptionServiceTests
+    {
+        [Fact]
+        public void EnsureEncryptedValueIsTheSameAsOriginal()
+        {
+            const string Value = "test";
+
+            var encryptionService = new DefaultEncryptionService();
+            var encryptedValue = encryptionService.Encrypt(Value);
+
+            encryptedValue.ShouldNotBeNull();
+            encryptedValue.Should().Be(Value);
+        }
+
+        [Fact]
+        public void EnsureDecryptedValueIsTheSameAsOriginal()
+        {
+            const string Value = "test";
+
+            var encryptionService = new DefaultEncryptionService();
+            var decryptedValue = encryptionService.Decrypt(Value);
+
+            decryptedValue.ShouldNotBeNull();
+            decryptedValue.Should().Be(Value);
+        }
+    }
+}
