23cf99624e7be5df5e61afc517777552b6949d5d
Bill Applegate <bill.applegate@gmail.com>
2020-09-15 17:03:59 -0400

    Updated the package to support .NET 5. Added an additional testing project that targets .NET 5.

---
+++ Projects/TestingFivePointZeroApplication/Controllers/ServiceController.cs
@@ -0,0 +1,32 @@
+﻿using AccessTokenClient;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TestingFivePointZeroApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServiceController : ControllerBase
+    {
+        private readonly ITokenClient client;
+
+        public ServiceController(ITokenClient client)
+        {
+            this.client = client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var tokenResponse = await client.RequestAccessToken(new TokenRequest
+            {
+                ClientIdentifier = "client",
+                ClientSecret     = "511536EF-F270-4058-80CA-1C89C192F69A",
+                Scopes           = new[] { "api1" },
+                TokenEndpoint    = "https://localhost:44303/connect/token"
+            });
+
+            return Ok(tokenResponse);
+        }
+    }
+}
