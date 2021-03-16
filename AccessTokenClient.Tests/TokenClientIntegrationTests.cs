7e390bf0a387f34718e37e90a6a9163b5d8d666e
Bill Applegate <bill.applegate@gmail.com>
2021-03-16 09:03:17 -0400

    Removed client identifier and secret.

--- AccessTokenClient.Tests/TokenClientIntegrationTests.cs
+++ AccessTokenClient.Tests/TokenClientIntegrationTests.cs
@@ -8,7 +8,7 @@ namespace AccessTokenClient.Tests
 {
     public class TokenClientIntegrationTests
     {
-        [Fact(Skip = "test")]
+        [Fact]
         public async Task EnsureClientCredentialsRequestSuccessful()
         {
             var services = new ServiceCollection();
@@ -24,8 +24,8 @@ namespace AccessTokenClient.Tests
             var tokenResponse = await tokenClient.RequestAccessToken(new TokenRequest
             {
                 TokenEndpoint    = "https://dev-85v9a1ld.auth0.com/oauth/token",
-                ClientIdentifier = "8sO5k8ECd5LlHEB2QZ2Dwji1zeeP1X7L",
-                ClientSecret     = "85578vmVYEXM_5LgkA2eyxsiEmRXa8gGlHJ9SQYfFBnefOnnKNfiEsDBeElCJks9",
+                ClientIdentifier = "",
+                ClientSecret     = "",
                 Scopes           = new[] { "features:read" }
             });
 
