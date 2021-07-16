3860d8df6e1f2b7a641bd949af2464c2af737bfd
wapplegate <bill.applegate@gmail.com>
2021-07-16 18:37:18 -0400

    Added unit test.

--- AccessTokenClient.Tests/TokenClientRetryTests.cs
+++ AccessTokenClient.Tests/TokenClientRetryTests.cs
@@ -3,7 +3,7 @@ using AccessTokenClient.Serialization;
 using AccessTokenClient.Tests.Helpers;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -26,26 +26,78 @@ namespace AccessTokenClient.Tests
             // Configure an http client with the default retry policy handler and a mock http message handler:
             services
                 .AddHttpClient("TestingClient")
-                .AddPolicyHandler(AccessTokenClientPolicy.GetDefaultRetryPolicy(new NullLogger<ITokenClient>()))
+                .AddPolicyHandler((p, _) => AccessTokenClientPolicy.GetDefaultRetryPolicy())
                 .AddHttpMessageHandler(() => mockHandler);
 
+            var provider = services.BuildServiceProvider();
+
             // Retrieve the configured client from the service provider:
-            var httpClient = services
-                .BuildServiceProvider()
+            var httpClient = provider
                 .GetRequiredService<IHttpClientFactory>()
                 .CreateClient("TestingClient");
 
-            var logger = new NullLogger<TokenClient>();
+            var logger = provider.GetRequiredService<ILogger<TokenClient>>();
             var tokenClient = new TokenClient(logger, httpClient, new ResponseDeserializer());
 
             try
             {
                 await tokenClient.RequestAccessToken(new TokenRequest
                 {
-                    TokenEndpoint    = "http://www.token-endpoint.com",
+                    TokenEndpoint = "http://www.token-endpoint.com",
                     ClientIdentifier = "client-identifier",
-                    ClientSecret     = "client-secret",
-                    Scopes           = new[] { "scope:read" }
+                    ClientSecret = "client-secret",
+                    Scopes = new[] { "scope:read" }
+                });
+            }
+            catch
+            {
+                // Swallowed purposefully.
+            }
+
+            // Currently, the retry policy is configured to retry twice, thus the total number of calls will be three:
+            mockHandler.NumberOfCalls.Should().Be(3);
+        }
+
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound)]
+        [InlineData(HttpStatusCode.RequestTimeout)]
+        [InlineData(HttpStatusCode.InternalServerError)]
+        public async Task EnsureTokenRequestRetriedOnFailureWhenLoggerSpecified(HttpStatusCode httpStatusCode)
+        {
+            var services = new ServiceCollection();
+
+            var mockHandler = new MockHttpMessageHandler(string.Empty, httpStatusCode);
+
+            services.AddLogging();
+
+            // Configure an http client with the default retry policy handler and a mock http message handler:
+            services
+                .AddHttpClient("TestingClient")
+                .AddPolicyHandler((p, _) =>
+                {
+                    var policyLogger = p.GetRequiredService<ILogger<TokenClient>>();
+                    return AccessTokenClientPolicy.GetDefaultRetryPolicy(policyLogger);
+                })
+                .AddHttpMessageHandler(() => mockHandler);
+
+            var provider = services.BuildServiceProvider();
+
+            // Retrieve the configured client from the service provider:
+            var httpClient = provider
+                .GetRequiredService<IHttpClientFactory>()
+                .CreateClient("TestingClient");
+
+            var logger = provider.GetRequiredService<ILogger<TokenClient>>();
+            var tokenClient = new TokenClient(logger, httpClient, new ResponseDeserializer());
+
+            try
+            {
+                await tokenClient.RequestAccessToken(new TokenRequest
+                {
+                    TokenEndpoint = "http://www.token-endpoint.com",
+                    ClientIdentifier = "client-identifier",
+                    ClientSecret = "client-secret",
+                    Scopes = new[] { "scope:read" }
                 });
             }
             catch
