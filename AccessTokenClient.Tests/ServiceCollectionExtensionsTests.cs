d4d1d5ae17db325a29311894ce495ef66bc3d231
wapplegate <bill.applegate@gmail.com>
2021-07-16 17:28:38 -0400

    Added additional unit tests for the extension package.

--- AccessTokenClient.Tests/ServiceCollectionExtensionsTests.cs
+++ AccessTokenClient.Tests/ServiceCollectionExtensionsTests.cs
@@ -3,6 +3,7 @@ using AccessTokenClient.Extensions;
 using AccessTokenClient.Tests.Helpers;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using Xunit;
 
 namespace AccessTokenClient.Tests
@@ -10,7 +11,7 @@ namespace AccessTokenClient.Tests
     public class ServiceCollectionExtensionsTests
     {
         [Fact]
-        public void EnsureServiceProviderReturnsTokenClientCachingDecoratorWhenCachingEnabled()
+        public void EnsureServiceProviderReturnsTokenClient()
         {
             var services = new ServiceCollection();
 
@@ -24,6 +25,25 @@ namespace AccessTokenClient.Tests
 
             client.ShouldNotBeNull();
             client.Should().BeOfType<TokenClientCachingDecorator>();
+        }
+
+
+        [Fact]
+        public void EnsureServiceProviderReturnsTokenClientWhenRetryPolicySpecified()
+        {
+            var services = new ServiceCollection();
+
+            services.AddAccessTokenClient(builder =>
+            {
+                builder.AddPolicyHandler(_ => AccessTokenClientPolicy.GetDefaultRetryPolicy());
+            });
+
+            var provider = services.BuildServiceProvider();
+
+            var client = provider.GetService<ITokenClient>();
+
+            client.ShouldNotBeNull();
+            client.Should().BeOfType<TokenClient>();
         }
 
         [Fact]
@@ -41,6 +61,48 @@ namespace AccessTokenClient.Tests
 
             client.ShouldNotBeNull();
             client.Should().BeOfType<TokenClient>();
+        }
+
+        [Fact]
+        public void EnsureExceptionThrownWhenInvalidCachingOptionsSpecified()
+        {
+            var services = new ServiceCollection();
+
+            Action invalidPrefixAction = () => services.AddAccessTokenClient().AddAccessTokenClientCache<MemoryTokenResponseCache>(options =>
+            {
+                options.CacheKeyPrefix = string.Empty;
+            });
+
+            invalidPrefixAction.Should().Throw<ArgumentException>();
+
+            Action invalidBufferAction = () => services.AddAccessTokenClient().AddAccessTokenClientCache<MemoryTokenResponseCache>(options =>
+            {
+                options.ExpirationBuffer = 0;
+            });
+
+            invalidBufferAction.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void EnsureExceptionThrownWhenServiceCollectionNullUsingAddAccessTokenClientExtensionMethod()
+        {
+            IServiceCollection services = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Action action = () => services.AddAccessTokenClient();
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void EnsureExceptionThrownWhenServiceCollectionNullUsingAddAccessTokenClientCacheMethod()
+        {
+            IServiceCollection services = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Action action = () => services.AddAccessTokenClientCache<MemoryTokenResponseCache>();
+
+            action.Should().Throw<ArgumentNullException>();
         }
     }
 }
